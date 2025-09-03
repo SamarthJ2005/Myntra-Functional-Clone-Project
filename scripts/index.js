070eb6b410aeae1097e2d5ac67559c47ebfe4210
SamarthJ2005 <samjalkote111@gmail.com>
2025-09-04 00:23:35 +0530

    adding bag javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
+let bagItems;
+onLoad();
+
+function onLoad(){
+    let bagItemsStr = localStorage.getItem('bagItems');
+    bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+displayItemsOnHomePage();
+displayBagIcon();
+}
+
+function addToBag(itemId) {
+    bagItems.push(itemId);
+    localStorage.setItem('bagItems', JSON.stringify(bagItems));
+    displayBagIcon();
+    
+}
+
+function displayBagIcon(){
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if (bagItems.length > 0 ) {
+        bagItemCountElement.style.visibility = 'visible';
+       bagItemCountElement.innerText = bagItems.length;}
+       else {
+        bagItemCountElement.style.visibility = 'hidden';
+       }
+    
+}
+
+function displayItemsOnHomePage() {
 let itemContainerElement = document.querySelector('.items-container');
 
 let innerHTML = '';
@@ -15,7 +44,8 @@ items.forEach(item => {
     <span class="original-price"> Rs ${item.original_price}</span>
     <span class="discount">${item.discount}% OFF</span>
     </div>
-    <button class="bag-button">Add to bag</button>
+    <button class="bag-button" onclick= "addToBag(${item.id})">Add to bag</button>
 </div> `
 });
 itemContainerElement.innerHTML = innerHTML;
+}
